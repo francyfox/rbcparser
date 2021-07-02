e20e44db266658b57880bc8a3d5e13b79228b9b9
francyfox <7info7web@gmail.com>
2021-07-02 11:32:05 +0600

    Initial commit

---
+++ src/Commands/runCronXmlParser.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Commands;
+
+
+use Symfony\Component\Console\Command\Command;
+
+class runCronXmlParser extends Command
+{
+//    protected function configure()
+//    {
+//        $this
+//            ->setName('cron:xml:parse')
+//            ->setDescription('Run cron task with parsing xml to DB');
+//
+//    }
+//
+//    public function execute(InputInterface $input, OutputInterface $output)
+//    {
+//        return Command::SUCCESS;
+//    }
+}
