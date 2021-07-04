703a649c765c6e870b662b6d1a50cdca8f1affa7
francyfox <7info7web@gmail.com>
2021-07-04 21:39:26 +0600

    cant check last modify and filesize. Need delete old files function

--- src/Services/xmlType.php
+++ src/Services/xmlType.php
@@ -21,24 +21,33 @@ class xmlType
         $this->home_path = $kernel->getProjectDir();
     }
 
-    function checkRssFileSize (string $url)
+    function deleteOldXml (array $filenames, string $newFilename)
     {
-        $file = $this->home_path.'/public/rss/news.rss';
-        if (!file_exists($file)) {
-            return false;
+
+    }
+
+    function xmlToArray (array $filenames): array
+    {
+        $mergedArray = array();
+        foreach ($filenames as $item) {
+            $xml = simplexml_load_file($item, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $json = json_decode(json_encode($xml), TRUE);
+            $array = array_pop($json);
+            array_push($mergedArray, $array);
         }
-        $ch = curl_init($url);
+        if (count($mergedArray) == 2) {
+            return array_diff_ukey($mergedArray[0], $mergedArray[1]);
+        } elseif (count($mergedArray) == 1) {
+            return $mergedArray[0];
+        }
+        return [];
+    }
 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_HEADER, TRUE);
-        curl_setopt($ch, CURLOPT_NOBODY, TRUE);
-
-        $data = curl_exec($ch);
-        $size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
-
-        curl_close($ch);
-        return $size == filesize($this->home_path.'/public/rss/news.rss');
-
+    function getLastTime (string $url): int
+    {
+        $date = new \DateTime('now');
+        $date = $date->format("M d Y H:i:s");
+        return strtotime($date);
     }
 
     function getXmlArray (string $url): array
@@ -46,45 +55,45 @@ class xmlType
         if (!file_exists($this->home_path.'/public/rss')) {
             mkdir($this->home_path.'/public/rss', 0700);
         }
-        $fp = fopen($this->home_path.'/public/rss/news.rss', 'w+');
+
+        $path = $this->home_path.'/public/rss/'.$this->getLastTime($url).'.rss';
+        $fp = fopen($path, 'c+');
         if($fp === false){
             throw new \Exception('Could not open: ' . $this->home_path.'/public/rss/news.rss');
         }
-        if ($this->checkRssFileSize($url)) {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11');
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_FILE, $fp);
-            if(!curl_exec($ch)){
-                curl_close($ch);
-                fclose($fp);
-                return false;
-            }
-            fflush($fp);
-            fclose($fp);
-            curl_close($ch);
 
-            $xml = simplexml_load_file($this->home_path.'/public/rss/news.rss', 'SimpleXMLElement', LIBXML_NOCDATA);
-            $json = json_decode(json_encode($xml), TRUE);
-            $array = array_pop($json);
-            return $array["item"];
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11');
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        if(!curl_exec($ch)){
+            curl_close($ch);
+            fclose($fp);
+            return [];
         }
-        return false;
+        fflush($fp);
+        fclose($fp);
+        curl_close($ch);
+
+        $map = glob($this->home_path.'/public/rss/*.rss');
+        return $this->xmlToArray($map);
     }
 
     function saveXmlArrayToDb(array $array)
     {
-        foreach ($array as $item) {
-            $news = new News();
-            $news
-                ->setTitle($item["title"])
-                ->setAnounce($item["description"])
-                ->setLink($item["link"])
-                ->setAuthor($item["author"] ?? null)
-                ->setImgUrl($item["enclosure"]["@attributes"]["url"] ?? null);
-            $this->em->persist($news);
-            $this->em->flush();
+        if (!empty($array)) {
+            foreach ($array as $item) {
+                $news = new News();
+                $news
+                    ->setTitle($item["title"])
+                    ->setAnounce($item["description"])
+                    ->setLink($item["link"])
+                    ->setAuthor($item["author"] ?? null)
+                    ->setImgUrl($item["enclosure"]["@attributes"]["url"] ?? null);
+                $this->em->persist($news);
+                $this->em->flush();
+            }
         }
     }
 }
